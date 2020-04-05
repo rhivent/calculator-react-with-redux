94864f675a2d081e9316775ba8c895c70ecda81f
Riventus <riventus@aksaramaya.com>
2020-04-05 21:48:43 +0700

    update reducers

--- src/reducers/calculator.js
+++ src/reducers/calculator.js
@@ -10,7 +10,7 @@ const INITIAL_STATE= {
   screen: '', history: []
 };
 
-const calculator=(state=INITIAL_STATE, action)=> {
+const cal=(state=INITIAL_STATE, action)=> {
   switch (action.type) {
       case PRESS_BUTTON:
         return {
@@ -18,17 +18,15 @@ const calculator=(state=INITIAL_STATE, action)=> {
           screen: (state.screen + '' + action.value)
         }
       case PRESS_EQUALS:
-        const newState= {
-          ...state
-        }
-        newState.screen=(eval(state.screen))
-        newState.history=state.history.concat(`${state.screen}`)
-        return newState
+        const newState= {...state};
+        newState.screen=(eval(state.screen));
+        newState.history=[...state.history,state.screen];
+        return newState;
       case PRESS_RESET:
         return {
           ...state,
           screen: ''
-      }
+        }
       case SELECT_HISTORY:
         return {
           ...state,
@@ -44,4 +42,4 @@ const calculator=(state=INITIAL_STATE, action)=> {
   }
 }
 
-export default calculator
+export default cal;
